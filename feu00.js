fef5041f6f18281e79e876780e4fb4d6ed4ae0ef
Replit user <>
2023-05-03 13:42:37 +0000

    feu00

--- feu00.js
+++ feu00.js
@@ -1,36 +1,45 @@
 /* Créez un programme qui affiche un rectangle dans le terminal.
 */
 
-function drawRectangle(longeur, largeur) {
-    let rectangle = "";
-    const cornerChar = 'o';
-    const lineChar = '-';
-    const sideChar = '|'
-
-    if (longeur === 1 && largeur === 1)
-        rectangle = cornerChar;
-    else if (largeur == 1) {
-        rectangle = cornerChar + lineChar.repeat(longeur -2) + cornerChar;
-
-    }
-    else if (longeur == 1) {
-        rectangle = cornerChar + '\n';
-        for (let i = 0; i < largeur - 2; i++) {
-            rectangle += sideChar + '\n';
-        }
-        rectangle += cornerChar;
-
-    }
-    else {
-        rectangle = cornerChar + lineChar.repeat(longeur -2) + cornerChar + '\n';
-    for (let i = 0; i < largeur - 2; i++) {
-        rectangle += sideChar + " ".repeat(longeur - 2) + sideChar +'\n';
-    }
-    rectangle += cornerChar + lineChar.repeat(longeur -2) + cornerChar;
-    }
-    
-
-    return rectangle;
+function isParamParse(width, height) {
+  if (!width || !height)
+    return false;
+  if (width < 1 || height < 1)
+    return false;
+  if (isNaN(Number(width)) || isNaN(Number(height)))
+    return false;
+  else
+    return true;
 }
 
-console.log(drawRectangle(1, 0));
+function drawRectangle(width, height) {
+  const corner = 'o';
+  const side = '|';
+  const line = '-';
+  
+  let drawing = "";
+
+  if (width == 1 && height == 1) {
+    draw = corner;
+  } else if (height == 1 && width > 1) {
+    draw = corner + line.repeat(width - 2) + corner;
+  } else if (width == 1 && height > 1) {
+    draw = corner + '\n';
+    for (let i = 0; i < height - 2; i++) {
+      draw += side + '\n';
+    }
+    draw += corner;
+  } else {
+    draw = corner + line.repeat(width - 2) + corner + '\n';
+    for (let i = 0; i < height - 2; i++) {
+      draw += side + " ".repeat(width -2) + side + '\n';
+    }
+    draw += corner + line.repeat(width - 2) + corner + '\n';
+  }
+  return draw;
+}
+
+myWidth = process.argv[2];
+myHeight = process.argv[3];
+
+isParamParse(myWidth, myHeight) ? console.log(drawRectangle(myWidth, myHeight)) : console.log("Erreur: argument invalide");
