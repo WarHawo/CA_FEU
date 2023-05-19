cc1304d96ff3d00d964a4f17961e2c81bbd57cc3
War <warhawo@gmail.com>
2023-05-19 18:47:59 +0200

    feu02.js

--- feu02.js
+++ feu02.js
@@ -1,53 +1,101 @@
 /* Programme qui affiche la position de l’élément le plus en haut à gauche (dans l’ordre) d’une forme au sein d’un plateau.
  */
 
-function readBoard(file) {
+function readBoardFile(file) {
   const fs = require("fs");
   
   if (fs.existsSync(file)) {
-      const data = fs.readFileSync(file, "utf-8");
-      return data;
+      const boardData = fs.readFileSync(file, "utf-8");
+      const lines = boardData.trim().split('\n');
+      for (let i = 1; i < lines.length; i++) {
+        if (lines[i].length !== lines[i - 1].length) {
+          console.error("Error: the board proportions are incorrect");
+          process.exit(1);
+        }
+      }
+      return lines.map(line => line.split(''));
   } else {
-    return "Error: the file doesn't exit";
+    console.log("Error: the bord file doesn't exit");
     process.exit(1);
   }
 }
 
-function parseBord(file) {
-  const data = readBoard(file);
-  let nbLines = 1;
-  
-  if (data.length === 0) {
-    return "Error: empty file";
+function readShapeFile(file) {
+  const fs = require("fs");
+  if (fs.existsSync(file)) {
+    const shapeData = fs.readFileSync(file, "utf-8");
+    if (shapeData.length === 0) {
+      console.log("Error the shape file souldn't be empty");
+      process.exit(1);
+    }
+    return shapeData.trim().split('\n');
+  } else {
+    console.log("Error: the shape file doesn't exit");
     process.exit(1);
   }
-  
-  for (let i = 0; i < data.length; i++) {
-    if (data[i] === '\n') {
-      nbLines++;
+}
+
+function findShapeInBoard(board, shape){
+  const boardHeight = board.length;
+  const bordWidth = board[0].length;
+  const shapeHeight = shape.length;
+  const shapeWidth = shape[0].length;
+
+  for(let row = 0; row <= boardHeight - shapeHeight; row++){
+    for (let col = 0; col <= bordWidth - shapeWidth; col++) {
+      let match = true;
+
+      for (let i = 0; i < shapeHeight; i++) {
+        for (let j = 0; j < shapeWidth; j++) {
+          if (shape[i][j] !== ' ' && shape[i][j] !== board[row + i][col + j]) {
+            match = false;
+            break;
+          }
+        }
+        if (!match) {
+          break;
+        }
+      }
+      if (match) {
+        return {row, col};
+      }
     }
   }
-  
-  let lines = new Array(nbLines)
-  for (let i = 0; i < nbLines; i++) {
-    lines[i] = new Array(1);
-  }
-  
-  let j = 0;
-  for (let i = 0; i < data.length; i++) {
-    if (data[i] !== '\n') {
-      lines[j] += data[i];
-    } else if (data[i] === '\n') {
-      j++;
+  return null;
+}
+
+function printResultBoard(board, shape, position){
+  const newBoard = [...board];
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      newBoard[i][j] = '-';
     }
   }
 
-  for (let i = 1; i < lines.length; i++) {
-    if (lines[i].length !== lines[i - 1].length) {
-      return 'Error: board format incorrect'
+  for (let i = 0; i < shape.length; i++) {
+    for (let j = 0; j < shape[i].length; j++) {
+      if (shape[i][j] !== ' ') {
+        newBoard[position.row + i][position.col + j] = shape[i][j]; 
+      } 
     }
   }
-  return lines;
+  console.log('Trouvé !');
+  console.log(`Coordonnées : ${position.row + 1},${position.col + 1}`);
+  console.log(newBoard.map(row => row.join('')).join('\n'));
 }
-myFile = process.argv[2];
-console.log(parseBord(myFile));
+
+function main () {
+  const bordFile = process.argv[2];
+  const shapeFile = process.argv[3];
+
+  const myBoard = readBoardFile(bordFile);
+  const myShape = readShapeFile(shapeFile);
+
+  let position = findShapeInBoard(myBoard, myShape);
+  if (position)
+    printResultBoard(myBoard, myShape, position);
+  else
+    console.log("Introuvable");
+}
+main();
