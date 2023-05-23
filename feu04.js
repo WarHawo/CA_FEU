7c3d9c3cc1d92d736d450a11bc25bffdec1f842e
Replit user <>
2023-05-23 12:08:11 +0000

    norm + message erreur modifier

--- feu04.js
+++ feu04.js
@@ -3,6 +3,7 @@
 
 function readBoardFile(file) {
   const fs = require('fs');
+  
   if (fs.existsSync(file)) {
     const boardData = fs.readFileSync(file, "utf-8");
     return boardData;
@@ -12,21 +13,21 @@ function readBoardFile(file) {
   }
 }
 
-function hasOnlyValidChar(array, emptyChar, obstaChar) {
-    for (let i = 0; i < array.length; i++) {
-        const elem = array[i];
-        if (elem != emptyChar && elem != obstaChar) {
-            return false;
-        }
+function hasOnlyValidChar(array, emptyChar, obstacleChar) {
+  for (let i = 0; i < array.length; i++) {
+    const elem = array[i];
+    if (elem != emptyChar && elem != obstacleChar) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 function parseBoard(board) {
   const lines = board.trim().split('\n');
   const infoLine = lines[0];
-  
-  const nbLines = parseInt(infoLine);
+
+  let nbLines = parseInt(infoLine);
   const chars = infoLine.match(/\D/g);
   if (isNaN(nbLines) || chars.length !== 3) {
     console.log("Error: first line incorrect");
@@ -34,30 +35,32 @@ function parseBoard(board) {
   }
 
   const emptyChar = chars[0];
-  const obstaChar = chars[1];
+  const obstacleChar = chars[1];
   const fullChar = chars[2];
 
-  lines.splice(0,1);
-  
+  lines.splice(0, 1);
+
   if (lines.length != nbLines) {
     console.log("Error: number of lines incorrect");
     process.exit(1);
   }
+  
   for (let i = 0; i < lines.length; i++) {
     let lineLength = lines[0].length;
     if (lines[i].length !== lineLength) {
       console.log("Error: line length incorrect");
       process.exit(1);
     }
-    if (!hasOnlyValidChar(lines[i], emptyChar, obstaChar)) {
+    if (!hasOnlyValidChar(lines[i], emptyChar, obstacleChar)) {
       console.log("Error: board char incorrect");
       process.exit(1);
     }
-  }  
-  return {board: lines.map(line => line.split('')), empty: emptyChar, obstacle: obstaChar, full: fullChar };
+  }
+  
+  return { board: lines.map(line => line.split('')), empty: emptyChar, obstacle: obstacleChar, full: fullChar };
 }
 
-function findLargestSquare(board, emptyChar, obstaChar) {
+function findLargestSquare(board, emptyChar, obstacleChar) {
   const numRows = board.length;
   const numCols = board[0].length;
   let largestSize = 0;
@@ -73,13 +76,13 @@ function findLargestSquare(board, emptyChar, obstaChar) {
         // Check if a square of larger size can be formed
         while (isSquare && size + row < numRows && size + col < numCols) {
           for (let i = row; i <= row + size; i++) {
-            if (board[i][col + size] === obstaChar) {
+            if (board[i][col + size] === obstacleChar) {
               isSquare = false;
               break;
             }
           }
           for (let j = col; j <= col + size; j++) {
-            if (board[row + size][j] === obstaChar) {
+            if (board[row + size][j] === obstacleChar) {
               isSquare = false;
               break;
             }
@@ -102,9 +105,9 @@ function findLargestSquare(board, emptyChar, obstaChar) {
   return { row: largestRow, col: largestCol, size: largestSize };
 }
 
-function fillLargestSquare(board, emptyChar,obstaChar, fullChar) {
-  const largestSquare = findLargestSquare(board, emptyChar, obstaChar);
-  const {row, col, size} = largestSquare;
+function fillLargestSquare(board, emptyChar, obstacleChar, fullChar) {
+  const largestSquare = findLargestSquare(board, emptyChar, obstacleChar);
+  const { row, col, size } = largestSquare;
 
   for (let i = row; i < row + size; i++) {
     for (let j = col; j < col + size; j++) {
@@ -127,7 +130,6 @@ function main() {
   const newBoard = fillLargestSquare(myBoard.board, myBoard.empty, myBoard.obstacle, myBoard.full);
 
   printBoard(newBoard);
-
 }
 
 main();
